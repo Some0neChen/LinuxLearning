3c89839a10df4ff8c73bbbbc62d5c2ed6d17ea59
some0nechen <1965669207@qq.com>
2025-12-21 11:20:26 +0000

    20251221_1

---
+++ Linux_4_ThreadManage/main.c
@@ -0,0 +1,7 @@
+#include "program_test.h"
+
+int main()
+{
+    testForkProgram();
+    return 0;
+}
---
+++ Linux_4_ThreadManage/program_test.h
@@ -0,0 +1,13 @@
+#ifndef __PROGRAM_TEST_H__
+#define __PROGRAM_TEST_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void testProgramPidPpid();
+void testForkProgram();
+
+#endif
